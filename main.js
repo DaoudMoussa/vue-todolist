e8e1c5da62dcfb514a6c268c8f8f26ed3ec9fc7a
Daoud <99m.daoud@gmail.com>
2020-11-11 11:22:14 +0100

    aggiunto evento click che cambia classe all'item cliccato NON FUNZIONA

--- main.js
+++ main.js
@@ -7,16 +7,36 @@ const app = new Vue({
             'palestra'
         ],
         doneList: [],
-        input: [],
-        nextTodoItem: ""
+        // input: [],
+        nextTodoItem: "",
+        statusArray: [],
+        // itemStatus: ""
     },
     methods: {
         addItem() {
             this.todoList.push(this.nextTodoItem);
+            this.statusArray.push("not-done");
             this.nextTodoItem = "";
         },
         addItemWEnter(event) {
             if(event.keyCode == 13) this.addItem();
+        },
+        toggleItem(index) {
+            console.log(index);
+            console.log(this.statusArray[index]);
+
+            if (this.statusArray[index] == "not-done") {
+                this.statusArray[index] = "done";
+            } else {
+                this.statusArray[index] = "not-done";
+            }
+
+            console.log(this.statusArray[index]);
+            // Perché non cambia classe???
         }
+    },
+    created() {
+        this.todoList.forEach(() => this.statusArray.push("not-done"))
+
     }
 });
