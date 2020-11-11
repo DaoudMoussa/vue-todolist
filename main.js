190e87d5afb0666464a844bfcaa160c239defead
Daoud <99m.daoud@gmail.com>
2020-11-11 10:39:40 +0100

    Aggiunta todo-list statica

---
+++ main.js
@@ -0,0 +1,15 @@
+const app = new Vue({
+    el: '#root',
+    data: {
+        todoList: [
+            'studiare',
+            'leggere',
+            'palestra'
+        ],
+        doneList: [],
+        input:[]
+    },
+    methods: {
+
+    }
+});
