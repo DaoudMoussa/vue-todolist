e653d96ef1898a1af9602f5a0befa93c8fb1f651
Daoud <99m.daoud@gmail.com>
2020-11-12 10:35:26 +0100

    fixato il bug degli spazi e cambiata struttura dati

--- main.js
+++ main.js
@@ -2,34 +2,43 @@ const app = new Vue({
     el: '#root',
     data: {
         todoList: [
-            'studiare',
-            'leggere',
-            'palestra'
+            {
+                text: 'studiare',
+                status: 'not-done'
+            },
+            {
+                text: 'leggere',
+                status: 'not-done'
+            },
+            {
+                text: 'palestra',
+                status: 'not-done'
+            }
         ],
         nextTodoItem: "",
-        statusArray: [],
     },
     methods: {
         addItem() {
+            this.nextTodoItem = this.nextTodoItem.trim();
             if(this.nextTodoItem.length > 3) {
-                this.todoList.push(this.nextTodoItem);
-                this.statusArray.push("not-done");
+                let newTodoObject = { text: this.nextTodoItem, status: 'not-done'}
+                this.todoList.push(newTodoObject);
             } else {
                 alert("l'item dev'essere di almeno 4 caratteri!!")
             }
             this.nextTodoItem = "";
         },
-        addItemWEnter(event) {
-            if(event.keyCode == 13) this.addItem();
-        },
         toggleItem(index) {
 
             let checkbox = document.getElementById('checkbox' + index);
-            if (this.statusArray[index] == "not-done") {
-                Vue.set(this.statusArray, index, 'done')
+            if (this.todoList[index].status == "not-done") {
+                // Vue.set(this.statusArray, index, 'done');
+                this.todoList[index].status = 'done';
                 checkbox.checked = true;
             } else {
-                Vue.set(this.statusArray, index, 'not-done')
+                this.todoList[index].status = 'not-done';
+
+                // Vue.set(this.statusArray, index, 'not-done');
                 checkbox.checked = false;
             }
 
@@ -37,8 +46,5 @@ const app = new Vue({
         getCheckboxId(index) {
             return "checkbox" + index;
         }
-    },
-    created() {
-        this.todoList.forEach(() => this.statusArray.push("not-done"))
     }
 });
